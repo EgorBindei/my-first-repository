c4918f8192f52b6e0a15151407c2c61e601fa29f
EgorBindei <egorbendei@gmail.com>
2025-10-04 11:54:22 +0300

    Добавил функцию сведений

---
+++ My_fierst_programm_git_C++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void printLanguageInfo() {
+    cout << "Эта программа написана на языке C++." << endl;
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    printLanguageInfo();
+    return 0;
+}
